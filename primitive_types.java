e7f93aba392a170f55ecc590abce63bb78966590
kojobeng954 <kojobeng954@gmail.com>
2026-06-14 00:22:40 +0000

    Learning about primitive types in java

---
+++ primitive_types.java
@@ -0,0 +1,16 @@
+public class primitive_types {
+    public static void main(String[] args) {
+        byte age = 20;
+        double GPA = 2.69;
+        long population = 33_000_000;
+
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Double.MAX_VALUE);
+
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+    }
+}
