433ce99b169cfed5f7da73bb49c1c484e23c13a9
kojobeng954 <kojobeng954@gmail.com>
2026-06-14 00:26:13 +0000

    Learning about how and when to use strings in Java

---
+++ strings.java
@@ -0,0 +1,9 @@
+public class strings {
+    public static void main(String[] args) {
+        String name = "Rice";
+        int quantity = 40;
+        float price = 28.99f;
+
+        System.out.printf("Name: %s, Quantity: %d, Price: %.2f$", name, quantity, price);
+    }
+}
