041691c3cb77097bfd2b0e95621a388640c3a27a
kojobeng954 <kojobeng954@gmail.com>
2026-06-14 00:21:21 +0000

    A little intro to types and variables in java

---
+++ types_and_variables.java
@@ -0,0 +1,15 @@
+public class types_and_variables {
+    public static void main(String[] args) {
+        int number = 3;
+        double decimal = 2.99;
+        boolean isAnAdult = true;
+        char single = 'A';
+        String word = "Word";
+
+        System.out.println(number);
+        System.out.println(decimal);
+        System.out.println(isAnAdult);
+        System.out.println(single);
+        System.out.println(word);
+    }
+}
