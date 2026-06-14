081a3a136948fd9a5131d7bd093b58138250d1f1
kojobeng954 <kojobeng954@gmail.com>
2026-06-14 00:29:32 +0000

    Learning how to create a 2d and 3d array in Java

---
+++ multi.java
@@ -0,0 +1,7 @@
+import javax.print.DocFlavor.STRING;
+
+public class multi {
+    public static void main(String[] args) {
+        String[][] board = { { "X", "O", "X" }, { "O", "X", "O" }, { "O", "O", "X" } };
+    }
+}
