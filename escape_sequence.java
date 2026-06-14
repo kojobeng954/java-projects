9c1e2eb60b23fcfbabb6a8a3e0b0709f50fb19f2
kojobeng954 <kojobeng954@gmail.com>
2026-06-14 00:27:35 +0000

    Learning about escape sequence in Java

---
+++ escape_sequence.java
@@ -0,0 +1,9 @@
+public class escape_sequence {
+    public static void main(String[] args) {
+        System.out.println("Ernest \t \t 20");
+        System.out.println("Kojo \t \t 25");
+        System.out.println("Ntow \t \t 30");
+        System.out.println("Gray \t \t 35");
+        System.out.println("Black \t \t 40");
+    }
+}
